8d0ebd816682c236853f124e6da2f2d6390cf210
bugratemirci <bugrahanetemircii@gmail.com>
2023-09-06 18:18:21 +0300

    add user state

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,6 +1,6 @@
 // Utilities
 import { defineStore } from "pinia";
-
+import User from "@/models/User.model";
 export const useAppStore = defineStore("app", {
   state: () => ({
     projectRootPath: {},
@@ -12,6 +12,21 @@ export const useAppStore = defineStore("app", {
   actions: {
     setProperties(config: any) {
       this.projectRootPath = config;
+    },
+  },
+});
+
+export const useUserStore = defineStore("app", {
+  state: () => ({
+    user: new User(),
+  }),
+
+  getters: {
+    getUsers: (state) => state,
+  },
+  actions: {
+    setProperties(user: any) {
+      this.user = user;
     },
   },
 });
